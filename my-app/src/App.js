47c2df9026e02292780f95cb7235c3a1a6541c88
mahesh <mahesh.auti@cuelogic.com>
2018-06-20 15:57:56 +0530

    done tilll css modules

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,70 +1,124 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
-  state = {
-    persons:[
-      { name : 'Mahi', age : 28 },
-      { name : 'mayu', age : 27 },
-      { name : 'manu', age : 26 }
-    ]
+  
+  state ={
+    person:[
+          {id:'as' , name:'sumit',age:22 },
+          {id:'sd' ,name:'mahesh',age:23},
+          {id:'df' , name:'sagar',age:25}
+        ]
   }
- swithNameHandler = (newName) => {
-  //console.log('Was Clicked');
-  //here we cant access persons state using this keyword like this.persons[0].name
- this.setState({
-  persons: [
-    { name : 'Mahesh', age : 38 },
-    { name : newName, age : 37 },
-    { name : 'manadi fanadi', age : 36 }
-  ]}
- );
- }
- nameChangeHandler = (event) => {
-   this.setState({
-    persons: [
-      { name : 'Mahesh', age : 38 },
-      { name : event.target.value, age : 37 },
-      { name : 'manadi fanadi', age : 36 }
-    ]
+  
+  switchNameHandler= (newname) =>
+  {
+    console.log("button clicked!!");
 
-   })
- }
+    this.setState({
+      person:[
+        { name:newname,age:22 },
+        {name:'mahesh',age:23},
+        {name:'sagar',age:30}
+      ]
 
-  render() {
-    //another method to add dynamic contents in state
-    //<button onClick={() => this.switchNameHandler('mahi!!')}>Switch name</button>
+    })
+  }
+
+  nameChangeHandler= (event,personId) =>
+  {
+      const personIndex=this.state.person.findIndex(p =>{
+        return p.id === personId;
+      } )
+
+     // const person=this.state.person[personIndex];//this return refferenece of array elment not value
+
+        const person={   //create new object and spred operator to get value
+          ...this.state.person[personIndex]
+        };
+
+        person.name=event.target.value;
+
+        const persons=[...this.state.person];
+        persons[personIndex]=person;
+        
+     this.setState({person:persons })
+  }
+  
+  togglePersonHandle= ()=>
+  {
+      const doesShow=this.state.showPersons;
+      this.setState({showPersons: !doesShow})
+  }
+
+  deletePersonHandler=(personindex)=>
+  {
+      //const person=this.state.person; dont use this approch it takes pointer
+      //const person=this.state.person.slice();//it copies all element person array to const person
+       const person=[...this.state.person];//spread operator to copy array elemment
+       person.splice(personindex,1);
+      this.setState({person:person});
+  }
+
+  render() 
+  {
+
     const style={
-      backgroundColor:'white',
-      font:'inherit',
-      border:'1x solid blue',
-      padding: '8px',
-      cursor:'pointer'
-     };
-    return (
-      <div className="App">
-      
-        <h1> hii, I am mahesh</h1>
-       <p>this is really working well</p>
-       <button 
-       style={style}
-       onClick={this.swithNameHandler.bind(this,'h1')}>Switch name</button>
+
+        backgroundColor:'green',
+        color:'white',
+        font:'inherit',
+        border:'1px solid blue',
+        padding:'8px',
+        cursor:'pointer',
        
-       <Person 
-          name = {this.state.persons[0].name} 
-          age = {this.state.persons[0].age}/>
-       <Person 
-          name = {this.state.persons[1].name} 
-          click ={this.swithNameHandler.bind(this,'h2')}
-          age = {this.state.persons[1].age}
-          changed={this.nameChangeHandler}></Person>
-       <Person 
-          name = {this.state.persons[2].name} 
-          age = {this.state.persons[2].age}/>
-      </div> 
+    }
+      let Persons=null;
+      if(this.state.showPersons)
+      {
+          Persons=(
+                <div>
+                  {
+                     this.state.person.map((person,index) => {
+                     return <Person 
+                          name={person.name} 
+                          age={person.age}
+                          click={()=>this.deletePersonHandler(index)}
+                          key={person.id}
+                          changed={(event)=>this.nameChangeHandler(event,person.id)}
+                          />     
+                    })
+                  }
+                </div> );
+              style.backgroundColor="red"
+              
+      }
+
+ let classes = [];
+ if(this.state.person.length<=2){
+   classes.push('red');
+
+ }
+ if(this.state.person.length<=1){
+   classes.push('bold');
+ }
+   
+ return(
+   
+      <div className="App">
+        <h1>hi , i am the react </h1>
+        <p className={classes.join(' ')}>this is working</p>
+        <button style={style} onClick={this.togglePersonHandle}/*{this.switchNameHandler.bind(this,'Maximelian')}*/>Toggle person</button>
+         {Persons}
+
+          
+      </div>
+     
+
+      //return  React.createElement('div',{className:'App'},React.createElement('h1',null,'does this work now'));
     );
-    // return React.createElement('div',{className:'App'},React.createElement('h1',null,'hey this is created using React'));
   }
 }
 
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,15 +1,21 @@
+
 import React from 'react';
 import './Person.css';
-//application components should change the state of application they should only render something to the dom
-//you can listen to the even in component and change the state in app.js
-const person = (props) => {
-    return(
-        <div className="Person">
-    <p onClick={props.click}> I'am {props.name} and i am {props.age} years old! </p>
-    <p>{props.children}</p>
-    <input type='text' onChange={props.changed} value={props.name}/>
-    </div>
-    ) 
-}
+// import Radium from 'radium';
+
+const person= (props) =>{
+// const style={
+//     '@media (min-width: 500px)':{
+//         width: '450px'
+//     }
+// }
+    return( 
+        <div className='Person'>
+            <p onClick={props.click}>I am {props.name}. i am {props.age} year old.</p> 
+            <p>{props.children}</p>
+            <input type='text' onChange={props.changed}  value={props.name}/>
+        </div>
+    );
+    };
 
 export default person;
